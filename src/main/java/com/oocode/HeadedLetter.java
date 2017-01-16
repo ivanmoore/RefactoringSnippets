e25f05d4c02f26e2e618159700fd0a69e098733f
Ivan Moore <ivan@teamoptimization.com>
2017-01-16 21:53:30 +0000

    added HeadedLetter class for refactoring demo

---
+++ src/main/java/com/oocode/HeadedLetter.java
@@ -0,0 +1,13 @@
+package com.oocode;
+
+import java.io.PrintStream;
+
+public class HeadedLetter {
+    public void print(PrintStream stream) {
+        stream.println("ACME Ltd");
+        stream.println("123 High St");
+        stream.println("London");
+        stream.println("SW12 1AA");
+        //...
+    }
+}
