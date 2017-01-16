0af816ea706c45b3c5ed648b389713b134b1ba41
Ivan Moore <ivan@teamoptimization.com>
2017-01-16 21:54:58 +0000

    removed unnecessary placeholder

--- src/test/java/com/oocode/ExampleTest.java
+++ src/test/java/com/oocode/ExampleTest.java
@@ -2,12 +2,9 @@ package com.oocode;
 
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
 public class ExampleTest {
     @Test
     public void canAnswerTheUniversalQuestion() {
-        assertThat(new Example().answer(), is(42));
+
     }
 }
